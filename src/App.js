ce6ceb300327a9cbeacbfee7a29b9199024a8072
SHIVAM SATYAM <78131073+Satyam3303@users.noreply.github.com>
2023-09-29 20:10:58 +0530

    Oh No Cringe

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import image from './Assets/oh no cringe.jpg';
+import songs from './Assets/oh no cringe.mp3';
 import './App.css';
 
 function App() {
+  const handlePlay = () => {
+    const audio = new Audio(songs);
+    audio.play()
+
+    const buttonElement = document.querySelector('.button'); 
+    if (buttonElement) {
+      buttonElement.style.display = 'none';
+    }
+
+    const imageElement = document.querySelector('.App-logo'); 
+    if (imageElement) {
+      imageElement.style.display = ''; 
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={image} style={{ display: 'none' }} className="App-logo" alt="logo" />
+        <button className='button' onClick={handlePlay}>Save Yourself From cringe</button>
       </header>
     </div>
   );
